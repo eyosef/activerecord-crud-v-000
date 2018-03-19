301834909cf7b00376b098c2eaf59a7f8e163176
Eden Mesfun <emesfun@gmail.com>
2018-03-19 01:58:19 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -37,7 +37,7 @@ def can_be_created_in_a_block(args = {})
 end
 
 def can_get_the_first_item_in_the_database
-  Movie.first.title #DOUBLE CHECK WITH COACHES
+  Movie.first.title
 end
 
 def can_get_the_last_item_in_the_database
